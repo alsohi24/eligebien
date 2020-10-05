b25d3600786a4f8ddb086d45d7095612d0211404
alvaro <a@yomail.com>
2020-10-04 21:06:46 -0500

    Definicion menu

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Lista!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Comparadores!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Instituciones académicas') {
+              iconName = focused ? 'university' : 'university';
+            } else if (route.name === 'Comparadores') {
+              iconName = focused ? 'area-chart' : 'area-chart';
+            }
+
+            // You can return any component that you like here!
+            //return <Ionicons name={iconName} size={size} color={color} />;
+            return <FontAwesome name={iconName} size={size} color={color} />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#250aa1',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Instituciones académicas" component={HomeScreen} />
+        <Tab.Screen name="Comparadores" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
